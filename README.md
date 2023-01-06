c699bc5bb2c890f311a0c203623319a18d0ad909
theredfish <did.julian@gmail.com>
2023-01-06 15:01:13 +0100

    ECS introduction

---
+++ README.md
@@ -0,0 +1,3 @@
+# ECS with bevy
+
+A repository to demonstrate ECS with Bevy for the blog post [put future link here]()
---
+++ src/main.rs
@@ -0,0 +1,26 @@
+use bevy::prelude::*;
+
+#[derive(Component)]
+struct Person;
+
+#[derive(Component)]
+struct Name(String);
+
+fn add_people(mut commands: Commands) {
+    commands.spawn((Person, Name("Elaina Proctor".to_string())));
+    commands.spawn((Person, Name("Renzo Hume".to_string())));
+    commands.spawn((Person, Name("Zayna Nieves".to_string())));
+}
+
+fn greet_people(query: Query<&Name, With<Person>>) {
+    for name in query.iter() {
+        println!("hello {}!", name.0);
+    }
+}
+
+fn main() {
+    App::new()
+        .add_startup_system(add_people)
+        .add_system(greet_people)
+        .run();
+}
